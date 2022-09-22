0e7c3c09ac872186acea37a7313921df525112cb
Valera <vareraj@gmail.com>
2022-09-22 19:38:13 +0300

    step 1.4

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,19 @@
 import webpack from 'webpack';
 
 export function buildLoaders(): webpack.RuleSetRule[] {
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      'style-loader',
+      // Translates CSS into CommonJS
+      'css-loader',
+      // Compiles Sass to CSS
+      'sass-loader',
+    ],
+  };
+
+  // Если не используем typescript - нужен babel-loader
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: 'ts-loader',
@@ -8,5 +21,5 @@ export function buildLoaders(): webpack.RuleSetRule[] {
   };
 
   /** порядок возвращения лоадеров имеет значение! */
-  return [typescriptLoader];
+  return [typescriptLoader, cssLoader];
 }
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { someFn } from './test';
-
-someFn(3);
-
-// document.body.innerHTML = `<h1>HeLLo</h1>`;
