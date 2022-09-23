188f616154cb8949747b879c5f1485c8f28949c3
Valera <vareraj@gmail.com>
2022-09-23 11:28:48 +0300

    step 1.6

---
+++ src/pages/MainPage/MainPage.async.tsx
@@ -0,0 +1,9 @@
+import { lazy } from 'react';
+export const MainPageAsync = lazy(
+  () =>
+    new Promise((resolve) => {
+      // @ts-ignore
+      // TODO: искуственная задержка, в продакте убрать
+      setTimeout(() => resolve(import('./MainPage')), 1500);
+    }),
+);
