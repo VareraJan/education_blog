db66bc1cf26402b7b05308f62a3dd01aab7b6945
Valera <vareraj@gmail.com>
2022-09-25 19:35:08 +0300

    step 2.2

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,17 @@
 import './styles/index.scss';
-import { Suspense, useContext, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Route, Routes } from 'react-router-dom';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPAge } from 'pages/MainPage';
 import { AppRouter } from './providers/router';
+import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={'/'}>Главная</Link>
-      <Link to={'/about'}>О нас</Link>
+      <Navbar />
       <AppRouter />
+      <button onClick={toggleTheme}>TOGGLE</button>
     </div>
   );
 };
