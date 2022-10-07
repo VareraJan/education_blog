3b953011f5eff949f6284be5dccb9d5cee8c091a
Valera <vareraj@gmail.com>
2022-10-07 12:41:36 +0300

    Lesson 20: add jest, add first test

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
+        jest: true,
     },
     extends: ['plugin:react/recommended', 'airbnb', 'plugin:i18next/recommended',
     ],
@@ -29,7 +30,7 @@ module.exports = {
         'no-shadow': 'off',
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'warn',
-        'max-len': ['warn', { code: 100 }],
+        'max-len': ['error', { ignoreComments: true, code: 100 }],
         'no-underscore-dangle': 'off',
         'i18next/no-literal-string': ['error', { markupOnly: true }],
     },
