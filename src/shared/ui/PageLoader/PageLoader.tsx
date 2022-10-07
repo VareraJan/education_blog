1fb571d6ae3475d4328302f544fc63aa68e5f56d
Valera <vareraj@gmail.com>
2022-10-07 16:44:30 +0300

    Lesson 21: add routing not found page, PageLoader

---
+++ src/shared/ui/PageLoader/PageLoader.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Loader } from '../Loader/Loader';
+import cls from './PageLoader.module.scss';
+
+interface PageLoaderProps {
+  className?: string;
+}
+
+export const PageLoader: React.FC<PageLoaderProps> = (props) => {
+    const { className } = props;
+
+    return (
+        <div className={classNames(cls.PageLoader, {}, [className])}>
+            <Loader />
+        </div>
+    );
+};
+// по сути это widget, по хорошему нужно перенести туда. Но не критично
