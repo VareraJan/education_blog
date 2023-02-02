688cb9298711c927c92a7bb8015e95523cf05b6a
Valera <vareraj@gmail.com>
2023-02-02 21:14:24 +0300

    add skeleton, code, article details card

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,20 +1,31 @@
+import { ArticleDetails } from 'entities/Article';
 import React, { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { useParams } from 'react-router-dom';
 import cls from './ArticleDetailsPage.module.scss';
 
 interface ArticleDetailsPageProps {
-  className?: string;
+    className?: string;
 }
 
 const ArticleDetailsPage: React.FC<ArticleDetailsPageProps> = (props) => {
     const { className } = props;
-    const { t } = useTranslation('article');
+    const { t } = useTranslation('article-details');
+    const { id } = useParams<{ id: string }>();
+
+    if (!id) {
+        return (
+            <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+                {t('Статья не найдена')}
+            </div>
+        );
+    }
 
     return (
         // eslint-disable-next-line i18next/no-literal-string
         <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
-            ARTICLE DETAIL
+            <ArticleDetails id={id} />
         </div>
     );
 };
--- src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
+++ src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
@@ -1,8 +1,6 @@
 import React, { memo } from 'react';
-import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import MainIcon from 'shared/assets/icons/main_20x20.svg';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { getUserAuthData } from 'entities/User';
