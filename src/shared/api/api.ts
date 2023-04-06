7d01ad600813d9dc24e6d33a69c40716d85d490b
Valera <vareraj@gmail.com>
2023-04-06 11:54:06 +0300

    fix api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -7,7 +7,12 @@ import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 // создаем инстанс аксиоса
 export const $api = axios.create({
     baseURL: __API__,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
-    },
+});
+
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+    }
+
+    return config;
 });
