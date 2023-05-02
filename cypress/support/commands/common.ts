75c857fe2d6410a49e7042d6a21f3c5ca72d7270
VareraJan <vareraj@gmail.com>
2023-05-02 16:24:49 +0300

    add tests for isolated component with cypress

--- cypress/support/commands/common.ts
+++ cypress/support/commands/common.ts
@@ -1,7 +1,6 @@
-import { selectByTestId } from 'cypress/helpers/selectByTestId';
-
 import { User } from '../../../src/entities/User';
 import { USER_LOCALSTORAGE_KEY } from '../../../src/shared/const/localstorage';
+import { selectByTestId } from '../../helpers/selectByTestId';
 
 export const login = (username: string = 'testuser', password:string = '123') => {
     return cy.request({
